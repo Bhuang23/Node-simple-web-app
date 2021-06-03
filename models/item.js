88663ade70abea51fd15a395f34ef5351036bf9c
Bhuang23 <59633478+Bhuang23@users.noreply.github.com>
2021-06-03 15:33:27 -0500

    updated user schema and item schema

--- models/item.js
+++ models/item.js
@@ -1,10 +1,13 @@
-var mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const itemSchema  = new mongoose.Schema({
     item_id: {type: Number, required: true},
     item_name: {type: String,required: true},
     item_price:{type: Number,required: true},
-    item_category:{type: String,required: true}
+    item_category:{type: String,required: true},
+    item_description: {type: String,required: true},
+    item_currency: {type: String,required: true},
+    item_image: {type: String,required: true}
 })
 
 
--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
-var mongoose = require('mongoose')
-
+const mongoose = require('mongoose');
 const userSchema  = new mongoose.Schema({
     username: {type: String,required: true},
     password:{type: String,required: true},
@@ -8,7 +7,16 @@ const userSchema  = new mongoose.Schema({
     address: {type: String,required: true},
     state: {type: String,required: false},
     country: {type: String,required: true},
-    zipcode: {type: String, required: true}
+    zipcode: {type: String, required: true},
+    orders: [{
+        item_id: Number,
+        item_name: String,
+        item_price: Number,
+        item_category: String,
+        item_description: String,
+        item_currency: String,
+        item_image: String
+    }]
 })
 
 
