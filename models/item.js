6aa7f9b67d374ca32be374a79d23c47252ba24b3
Bhuang23 <59633478+Bhuang23@users.noreply.github.com>
2021-07-24 16:48:46 -0500

    change item_category type from string to array of strings

--- models/item.js
+++ models/item.js
@@ -4,7 +4,7 @@ const itemSchema  = new mongoose.Schema({
     item_id: {type: Number, required: true},
     item_name: {type: String,required: true},
     item_price:{type: Number,required: true},
-    item_category:{type: String,required: true},
+    item_category:[{type: String,required: true}],
     item_description: {type: String,required: true},
     item_currency: {type: String,required: true},
     item_image: {type: String,required: true},
