c113851bc980485cc1279e39e1836b73e6ba76ef
Bhuang23 <59633478+Bhuang23@users.noreply.github.com>
2021-12-03 06:49:01 -0600

    updated cors origin

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(cors({origin: ['http://localhost:3000', 'https://daoggregate.netlify.app']}));
+app.use(cors({origin: ['http://localhost:3000', 'https://shoppingfirst.herokuapp.com/']}));
 app.use('/users', studentRoute)
 app.use('/shop', shopRoute)
 app.use('/review', reviewRoute)
