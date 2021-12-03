98ad9d6e9090ef9cc688afc94b052aa87bcf6b37
Bhuang23 <59633478+Bhuang23@users.noreply.github.com>
2021-12-03 09:06:00 -0600

    updated cors origin

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(cors({origin: ['https://shoppingfirst.netlify.app']}));
+app.use(cors({origin: ['http://localhost:3000','https://shoppingfirst.netlify.app']}));
 app.use('/users', studentRoute)
 app.use('/shop', shopRoute)
 app.use('/review', reviewRoute)
